63e2dbc2942a3bdc18c3fbf4357351125a9fb3a6
elvisanthonyy <info.elvisanthony@gmail.com>
2025-11-27 17:05:04 +0100

    fix loading ish

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 const Lodaing = () => {
   return (
     <div className="fixed w-full h-dvh top-0 left-0 flex justify-center items-center">
-      <div className="w-10 h-10 rounded-full border border-transparent border-t-blue-700"></div>
+      <div className="w-10 h-10 rounded-full border-2 border-transparent animate-spin border-t-blue-700"></div>
     </div>
   );
 };
