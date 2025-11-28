2cd8833d1361f7e7c02ce8332f4aa6cb20479c95
elvisanthonyy <info.elvisanthony@gmail.com>
2025-11-28 12:39:33 +0100

    mail

--- libs/sendResetMessage.ts
+++ libs/sendResetMessage.ts
@@ -20,11 +20,11 @@ export async function sendResetMessage(email: string, resetToken: string) {
       from: `"UC DOM" <${process.env.EMAIL_USER}>`,
       to: email,
       subject: "Reset Password",
-      html: `<div style="width: 100%; display: flex; height: 100px; flex-direction: column; align-items: center;"><h2 style="font: bold; margin-bottom: 30px; color: #03a3ff;"> UC DOM </h2>
-      <div><h3 style="margin-bottom: 50px">Your reset link</h3>
+      html: `<div style="width: 100%; display: flex; height: 500px; flex-direction: column; align-items: center;"><h2 style="font: bold; margin-bottom: 30px; color: #03a3ff;"> UC DOM </h2>
+      <h3 style="margin-bottom: 50px">Your reset link</h3>
       <p style="margin-bottom: 50px">This is your reset link <a style="color: #03a3ff;" href="${process.env.BASE_URL}/user/reset-password?token=${resetToken}">Reset Password</a></p> 
       <p style="margin-bottom: 30px;">Expires in 1 hour </p>
-      <p>Contact 09045342672 on WhatsApp for help info</p></div>
+      <p>Contact 09045342672 on WhatsApp for help info</p>
       </div>
       `,
       headers: {},
